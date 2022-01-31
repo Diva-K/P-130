dfd5bcf29660dde68c4f9a785036edbc61d54937
Diva-K <69714817+Diva-K@users.noreply.github.com>
2022-01-31 19:07:25 +0530

    Add files via upload

---
+++ datacleaner.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import csv
+df=pd.read_csv("total_stars.csv")
+df.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
+final_data=df.dropna()
+df.to_csv("final.csv")
+
